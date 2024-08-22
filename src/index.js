5b897c81779a5e3ca206bf26eab92c7b43456149
BrustS <xbrust88x@gmail.com>
2024-08-22 21:45:54 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let arrayUnit = String(n).split('')
+    arrayUnit = arrayUnit.reverse()
+    let result = arrayUnit.join('')
+    return parseInt(result)
 }
